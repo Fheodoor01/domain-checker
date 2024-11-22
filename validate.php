9540aa930d816e4817e0a2bdb1a0c7eaf05ef13c
Pieter <pieter@netflux.nl>
2024-11-22 17:06:56 +0100

    Go

--- validate.php
+++ validate.php
@@ -1,19 +1,12 @@
 <?php
 
 function validateDomain($domain) {
-    // Try dig command first as it's more reliable for DNSSEC
-    $hasDnssec = checkDnssecWithDig($domain);
-    if ($hasDnssec) {
-        return true;
-    }
-
-    // Fallback to PHP method if dig fails
     return checkDnssecWithPhp($domain);
 }
 
 function checkDnssecWithPhp($domain) {
     try {
-        // Only use DNS_A type as it's guaranteed to exist
+        // Try DNS_A first as it's most commonly supported
         $records = @dns_get_record($domain, DNS_A);
         if (!empty($records)) {
             foreach ($records as $record) {
@@ -22,67 +15,44 @@ function checkDnssecWithPhp($domain) {
                 }
             }
         }
-        
+
+        // Try DNS_NS records
+        $ns_records = @dns_get_record($domain, DNS_NS);
+        if (!empty($ns_records)) {
+            foreach ($ns_records as $record) {
+                if (isset($record['type']) && $record['type'] === 'RRSIG') {
+                    return true;
+                }
+            }
+        }
+
+        // Try DNS_SOA records
+        $soa_records = @dns_get_record($domain, DNS_SOA);
+        if (!empty($soa_records)) {
+            foreach ($soa_records as $record) {
+                if (isset($record['type']) && $record['type'] === 'RRSIG') {
+                    return true;
+                }
+            }
+        }
+
         // Try DNS_ANY if available
         if (defined('DNS_ANY')) {
             $any_records = @dns_get_record($domain, DNS_ANY);
             if (!empty($any_records)) {
                 foreach ($any_records as $record) {
-                    if (isset($record['type']) && 
-                        (strpos($record['type'], 'RRSIG') !== false || 
-                         strpos($record['type'], 'DNSKEY') !== false || 
-                         strpos($record['type'], 'DS') !== false)) {
-                        return true;
+                    if (isset($record['type'])) {
+                        $type = strtoupper($record['type']);
+                        if ($type === 'RRSIG' || $type === 'DNSKEY' || $type === 'DS') {
+                            return true;
+                        }
                     }
                 }
             }
         }
+
     } catch (Exception $e) {
-        // If PHP method fails, we already tried dig
-        return false;
-    }
-    
-    return false;
-}
-
-function checkDnssecWithDig($domain) {
-    try {
-        // First, check for DNSKEY records
-        $command = sprintf('dig +dnssec +noall +answer %s DNSKEY', escapeshellarg($domain));
-        exec($command, $output, $return_var);
-
-        if ($return_var === 0 && !empty($output)) {
-            foreach ($output as $line) {
-                if (strpos($line, 'DNSKEY') !== false || strpos($line, 'RRSIG') !== false) {
-                    return true;
-                }
-            }
-        }
-
-        // Then check for DS records
-        $command = sprintf('dig +noall +answer %s DS', escapeshellarg($domain));
-        exec($command, $output, $return_var);
-
-        if ($return_var === 0 && !empty($output)) {
-            foreach ($output as $line) {
-                if (strpos($line, 'DS') !== false) {
-                    return true;
-                }
-            }
-        }
-
-        // Finally, check for RRSIG records
-        $command = sprintf('dig +dnssec +noall +answer %s SOA', escapeshellarg($domain));
-        exec($command, $output, $return_var);
-
-        if ($return_var === 0 && !empty($output)) {
-            foreach ($output as $line) {
-                if (strpos($line, 'RRSIG') !== false) {
-                    return true;
-                }
-            }
-        }
-    } catch (Exception $e) {
+        // If any errors occur, assume no DNSSEC
         return false;
     }
 
