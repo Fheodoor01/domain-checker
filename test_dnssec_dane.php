243e803d571fd5f4e244270fb2cbb6e539f2ba56
Pieter <pieter@netflux.nl>
2024-11-22 15:57:07 +0100

    Go

--- test_dnssec_dane.php
+++ test_dnssec_dane.php
@@ -1,5 +1,9 @@
 <?php
 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once 'netdns2-masterNet/DNS2.php';
 
 function queryDnsServer($domain, $type, $dnsServer) {
